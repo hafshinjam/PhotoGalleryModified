a41e1e102a13795241e3005705cb2a92c0b9a4d0
ctrlslash <1ace28d67b>
2020-10-21 12:55:45 +0330

    Testing bind service like google sameple.

--- app/src/main/java/org/maktab/photogallery/services/LocalService.java
+++ app/src/main/java/org/maktab/photogallery/services/LocalService.java
@@ -3,9 +3,15 @@ package org.maktab.photogallery.services;
 import android.app.Service;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Binder;
 import android.os.IBinder;
 
+import java.util.Random;
+
 public class LocalService extends Service {
+
+    private IBinder mBinder = new LocalBinder();
+    private Random mGenerateRandom = new Random();
 
     public static Intent newIntent(Context context) {
         return new Intent(context, LocalService.class);
@@ -15,16 +21,36 @@ public class LocalService extends Service {
     }
 
     @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+    }
+
+    @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
         boolean isFinish = intent.getBooleanExtra("isFinish", false);
         if (isFinish)
             stopSelf();
-        return super.onStartCommand(intent, flags, startId);
+
+        return Service.START_REDELIVER_INTENT;
     }
 
     @Override
     public IBinder onBind(Intent intent) {
-        // TODO: Return the communication channel to the service.
-        throw new UnsupportedOperationException("Not yet implemented");
+        return mBinder;
+    }
+
+    public int getRandomNumber() {
+        return mGenerateRandom.nextInt(100);
+    }
+
+    public class LocalBinder extends Binder {
+        public LocalService getLocalService() {
+            return LocalService.this;
+        }
     }
 }
--- app/src/main/java/org/maktab/photogallery/view/activity/PhotoGalleryActivity.java
+++ app/src/main/java/org/maktab/photogallery/view/activity/PhotoGalleryActivity.java
@@ -1,8 +1,14 @@
 package org.maktab.photogallery.view.activity;
 
+import android.content.ComponentName;
+import android.content.Context;
 import android.content.Intent;
+import android.content.ServiceConnection;
 import android.os.Bundle;
+import android.os.IBinder;
+import android.util.Log;
 import android.view.View;
+import android.widget.Toast;
 
 import androidx.fragment.app.Fragment;
 
@@ -11,6 +17,8 @@ import org.maktab.photogallery.view.fragment.PhotoGalleryFragment;
 
 public class PhotoGalleryActivity extends SingleFragmentActivity {
 
+    private LocalService mLocalService;
+    private boolean mBound = false;
     int number = 0;
 
     @Override
@@ -27,12 +35,56 @@ public class PhotoGalleryActivity extends SingleFragmentActivity {
 
                 startService(intent);
                 number++;
+
+                if (mBound) {
+                    int random = mLocalService.getRandomNumber();
+                    Toast.makeText(
+                            PhotoGalleryActivity.this,
+                            "random: " + random,
+                            Toast.LENGTH_SHORT).show();
+                }
             }
         });
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        Intent intent = LocalService.newIntent(PhotoGalleryActivity.this);
+        bindService(intent, mConnection, Context.BIND_AUTO_CREATE);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+
+        unbindService(mConnection);
+        mBound = false;
     }
 
     @Override
     public Fragment createFragment() {
         return PhotoGalleryFragment.newInstance();
     }
+
+    private ServiceConnection mConnection = new ServiceConnection() {
+        @Override
+        public void onServiceConnected(ComponentName name, IBinder service) {
+            if (!(service instanceof LocalService.LocalBinder)) {
+                Log.e("Service", "This is wrong service");
+                return;
+            }
+
+            mBound = true;
+            LocalService.LocalBinder localBinder = (LocalService.LocalBinder) service;
+            mLocalService = localBinder.getLocalService();
+            mBound = true;
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName name) {
+            mBound = false;
+        }
+    };
 }
