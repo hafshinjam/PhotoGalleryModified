b9324174699b00d30319a07ba4297d3dad154375
ctrlslash <1ace28d67b>
2020-10-28 13:58:41 +0330

    Using Work Manager instead of other scheduling managers. Adding new Worker named PollWorker, schedule a worker.

---
+++ app/src/main/java/org/maktab/photogallery/worker/PollWorker.java
@@ -0,0 +1,84 @@
+package org.maktab.photogallery.worker;
+
+import android.content.Context;
+import android.util.Log;
+
+import androidx.annotation.NonNull;
+import androidx.work.Constraints;
+import androidx.work.ExistingPeriodicWorkPolicy;
+import androidx.work.NetworkType;
+import androidx.work.PeriodicWorkRequest;
+import androidx.work.WorkInfo;
+import androidx.work.WorkManager;
+import androidx.work.Worker;
+import androidx.work.WorkerParameters;
+
+import org.maktab.photogallery.utilities.Services;
+
+import java.util.List;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
+
+public class PollWorker extends Worker {
+
+    public static final String TAG = "PollWorker";
+    private static final String WORK_TAG_POLL = "PhotoPollWorker";
+
+    public PollWorker(@NonNull Context context, @NonNull WorkerParameters workerParams) {
+        super(context, workerParams);
+    }
+
+    @NonNull
+    @Override
+    public Result doWork() {
+        Services.pollFromServerAndNotify(getApplicationContext(), TAG);
+        return Result.success();
+    }
+
+    public static void scheduleWork(Context context, boolean isOn) {
+        Constraints constraints = new Constraints.Builder()
+                .setRequiredNetworkType(NetworkType.UNMETERED)
+                .build();
+
+        PeriodicWorkRequest workRequest =
+                new PeriodicWorkRequest.Builder(PollWorker.class, 15, TimeUnit.MINUTES)
+                        .setConstraints(constraints)
+                        .build();
+
+        if (isOn) {
+            Log.d(TAG, "enqueue work");
+            WorkManager
+                    .getInstance(context)
+                    .enqueueUniquePeriodicWork(
+                            WORK_TAG_POLL,
+                            ExistingPeriodicWorkPolicy.KEEP,
+                            workRequest);
+        } else {
+            Log.d(TAG, "cancel work");
+            WorkManager
+                    .getInstance(context)
+                    .cancelAllWorkByTag(WORK_TAG_POLL);
+        }
+    }
+
+    public static boolean isWorkScheduled(Context context) {
+        try {
+            WorkManager workManager = WorkManager.getInstance(context);
+            List<WorkInfo> workInfos = workManager.getWorkInfosByTag(WORK_TAG_POLL).get();
+
+            for (WorkInfo workInfo: workInfos) {
+                if (workInfo.getState() == WorkInfo.State.ENQUEUED ||
+                        workInfo.getState() == WorkInfo.State.RUNNING)
+                    return true;
+            }
+
+            return false;
+        } catch (ExecutionException e) {
+            Log.e(TAG, e.getMessage(), e);
+            return false;
+        } catch (InterruptedException e) {
+            Log.e(TAG, e.getMessage(), e);
+            return false;
+        }
+    }
+}
